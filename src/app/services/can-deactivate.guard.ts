277c961fcc1176b528839443ec7b9e4b8c9e1395
pegonsLog <pegons.log@gmail.com>
2024-03-26 21:42:31 -0300

    26/03/2024 - Casa

--- src/app/services/can-deactivate.guard.ts
+++ src/app/services/can-deactivate.guard.ts
@@ -1,12 +1,15 @@
-import { ActivatedRoute, CanDeactivateFn } from '@angular/router';
+import { CanDeactivateFn } from '@angular/router';
 import { HomeComponent } from '../modules/home/home.component';
-import { inject } from '@angular/core';
 
 export const canDeactivateGuard: CanDeactivateFn<HomeComponent> = (component, currentRoute, currentState, nextState) => {
 
   if(!component.isLogged){
-    nextState.root.data
-    return confirm('Você deseja sair');
+
+    const conf = confirm('Você deseja sair');
+    if(conf){
+      return true;
+    }else{
+      return false;
+    }
   }
-    return true;
 };
